3717d6dbeb6ce7c22ec50a9d0bae152907ae43ef
Mustafa Yurtdas <myurtdas06@gmail.com>
2023-03-13 11:03:14 +0300

    jdbc

---
+++ src/main/java/Query02.java
@@ -0,0 +1,47 @@
+import java.sql.*;
+
+public class Query02 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+
+        // 1) Driver Tanımla
+        Class.forName("org.postgresql.Driver");
+
+        // 2) Database'e Bağlan
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/JDBC",
+                "postgres",
+                "pmyy.78530813");
+
+        // 3) Statement
+        Statement st = con.createStatement();
+
+
+        //Soru: Region id'si 1 olan "country name" değerlerini çağırın.
+
+        String sql01 = "select country_name from countries where region_id=1";
+
+        //4) ResultSet
+        ResultSet data = st.executeQuery(sql01);
+
+        while (data.next()) {
+            System.out.println(data.getString(1));
+        }
+
+        //Soru: "region_id"nin 2'den büyük olduğu "country_id" ve "country_name" değerlerini çağırın.
+
+        String sql02 = "select country_id, country_name from countries where region_id>2";
+        ResultSet data2 = st.executeQuery(sql02);
+
+        while (data2.next()) {
+
+            System.out.println(data2.getString("country_id") + data2.getString("country_name"));
+        }
+
+        con.close();
+        st.close();
+        data.close();
+        data2.close();
+
+    }
+}
---
+++ src/main/java/Query03.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Query03 {
+
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/JDBC",
+                "postgres",
+                "pmyy.78530813");
+        Statement st = con.createStatement();
+/*
+        //"workers" adında bir table oluşturup "worker_id, worker_name, worker_salary" sütunlarını ekleyin.
+        String sql01 = "Create Table workers (worker_id Varchar(10), worker_name Varchar(20), worker_salary Int)";
+        st.execute(sql01);
+
+        //Table'a worker_address sütunu ekleyin.
+        String sql02 = "Alter Table workers add worker_address Varchar(50)";
+        st.execute(sql02);
+
+        // workers table'ını silin.
+        String sql03 = "Drop Table workers";
+        st.execute(sql03);
+*/
+        
+
+
+    }
+}
