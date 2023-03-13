0994a997e7366f9f48ce3a032a36abfeb927922d
Mustafa Yurtdas <myurtdas06@gmail.com>
2023-03-13 12:40:31 +0300

    jdbc

--- src/main/java/Query04.java
+++ src/main/java/Query04.java
@@ -1,4 +1,32 @@
-public class Query04 {
+import java.sql.*;
 
+public class Query04 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/JDBC",
+                "postgres",
+                "pmyy.78530813");
+        Statement st = con.createStatement();
+
+        ResultSet rs = st.executeQuery("select * from ogrenciler");
+
+        ResultSetMetaData rsmd = rs.getMetaData();
+        System.out.println("Sütun  sayısı: "+rsmd.getColumnCount());
+        System.out.println("1. sütun : "+ rsmd.getColumnName(1));
+        System.out.println("2. sütun : "+ rsmd.getColumnName(2));
+        System.out.println("3. sütun : "+ rsmd.getColumnName(3));
+        System.out.println("4. sütun : "+ rsmd.getColumnName(4));
+
+        System.out.println("Tablo Ismi = " + rsmd.getTableName(2));
+
+        System.out.println("rsmd.getColumnType(1) = " + rsmd.getColumnTypeName(1));
+        System.out.println("rsmd.getColumnType(1) = " + rsmd.getColumnTypeName(2));
+        System.out.println("rsmd.getColumnType(1) = " + rsmd.getColumnTypeName(3));
+        System.out.println("rsmd.getColumnType(1) = " + rsmd.getColumnTypeName(4));
+
+
+    }
 
 }
